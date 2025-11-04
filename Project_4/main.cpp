c11a606e913859d48d42cc264d68a699d3e6c174
Kyle <kylerockwell9@gmail.com>
2025-11-03 20:27:30 -0800

    project 4 started

---
+++ Project_4/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+//make sure files exist and can be written
+
+
+
+//function to read header file, and store pairs in std::vector<<std::pair<<std::string, std::string>>
+readHeader(ostream inputFile, std::vector<std::pair<std::string, std::string>> header){
+	
+}
+
+
+
+
+
+
+// create a huffman decoder tree using the vector.
+
+
+int main(){
+
+	
+
+
+
+	return 0;
+}
